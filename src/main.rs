ec3fc8c0a3a45891f95da825d0314602c5d8d829
Miracle Friday <miraclef60@gmail.com>
2023-12-07 21:19:03 +0100

    feat: implemented 2 get request handlers, 1 with path param

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+// Implement handlers for requests
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello, index!"
+}
+
+#[get("/{name}")]
+async fn hello(name: web::Path<String>, iden: web::Query<String>) -> impl Responder {
+    format!("Hello, {name}, You are {iden}")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("...Initializing web server ");
+
+    HttpServer::new(|| App::new().service(index).service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
